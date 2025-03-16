9ac3c0853eb010d9db7d50d1c333a35d5d39b2b7
Yugal <pathakyugal7@gmail.com>
2025-03-16 23:18:24 +0530

    [Yugal] Add.Login and Register with JWT code in UC5 branch

---
+++ RepositoryLayer/Migrations/20250316174544_UpdatedMigration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RepositoryLayer.Migrations
+{
+    public partial class UpdatedMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordHash",
+                table: "AddressBookEntries",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "AddressBookEntries");
+        }
+    }
+}
